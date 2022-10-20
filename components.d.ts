d80fd94d3323c7f97aa3f85b066058c427e84676
coderwhy <coderwhy@gmail.com>
2022-10-20 20:37:08 +0800

    type_check

--- components.d.ts
+++ components.d.ts
@@ -45,9 +45,6 @@ declare module '@vue/runtime-core' {
     HeaderInfo: typeof import('./src/components/nav-header/c-cpns/header-info.vue')['default']
     NavHeader: typeof import('./src/components/nav-header/nav-header.vue')['default']
     NavMenu: typeof import('./src/components/nav-menu/nav-menu.vue')['default']
-    PageContent: typeof import('./src/components/page-content/page-content.vue')['default']
-    PageModal: typeof import('./src/components/page-modal/page-modal.vue')['default']
-    PageSearch: typeof import('./src/components/page-search/page-search.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
